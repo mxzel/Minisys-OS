47a55942cb406f3e5388678058662524286acb0d
zmx976508106 <976508106@qq.com>
2018-10-27 15:36:58 +0800

    add Lab05

---
+++ docs/example programs/Lab05_7seg/VerilogFiles/mipsfpga_ahb_sevensegdec.v
@@ -0,0 +1,10 @@
+/*
+ * Seven-segment display decoder
+ */
+
+module mipsfpga_ahb_sevensegdec(input      [3:0] data,
+                                output reg [6:0] segments);
+
+  // Add code here.
+
+endmodule
