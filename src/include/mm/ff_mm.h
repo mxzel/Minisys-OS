a280c9f5677b01353bb6d2e8a0d14c0a5e776700
zmx976508106 <976508106@qq.com>
2018-11-18 21:24:48 +0800

    sync

---
+++ src/include/mm/ff_mm.h
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ff_mm.h
+ *
+ *    Description:  First-Fit 物理内存管理子系统
+ *
+ *        Version:  1.0
+ *        Created:  2014年11月05日 11时36分16秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Qianyi.lh (liuhuan), qianyi.lh@alibaba-inc.com
+ *        Company:  Alibaba-Inc Aliyun
+ *
+ * =====================================================================================
+ */
+
+#ifndef _INCLUDE_MM_FF_MM_H
+#define _INCLUDE_MM_FF_MM_H
+
+// 首次适应算法
+extern struct pmm_manager ff_mm_manager;
+
+#endif  // _INCLUDE_MM_FF_MM_H
--- src/include/mm/pmm.h
+++ src/include/mm/pmm.h
@@ -17,48 +17,16 @@
 #define PMM_MAX_SIZE 0x0003FFFC
 
 // 内核在物理内存中的起始位置
-#define RAM_KERNEL_START 0x00000000
+// #define RAM_KERNEL_START 0x00000000
 
 // 内核在物理内存中的结束位置
-#define RAM_KERNEL_STOP 0x0003FFFC
+// #define RAM_KERNEL_STOP 0x0003FFFC
 
 // 内核代码在内存中的起始和结束位置，在链接脚本中定义
-extern uint8_t kern_start[];
-extern uint8_t kern_end[];
+// extern uint8_t kern_start[];
+// extern uint8_t kern_end[];
 
 
-// 开启分页机制之后的内核栈
-extern uint8_t kern_stack[STACK_SIZE];
-
-// 内核栈的栈顶
-extern uint32_t kern_stack_top;
-
-// BIOS int 0x15 AX = 0xE820 常量
-// #define E820MAX             (20)      // 最大的表项数目
-// #define E820_ARM            (1)       // 可用 RAM
-// #define E820_ARR            (2)       // 保留区域
-
-// typedef struct e820map_t {
-//         uint32_t count;
-//         struct {
-//                 uint32_t addr_low;
-//                 uint32_t addr_high;
-//                 uint32_t length_low;
-//                 uint32_t length_high;
-//                 uint32_t type;
-//         } __attribute__((packed)) map[E820MAX];
-// } e820map_t;
-
-// 内存页类型
-// typedef
-// enum mem_zone_t {
-//         ZONE_DMA = 0,
-//         ZONE_NORMAL = 1,
-//         ZONE_HIGHMEM = 2
-// } mem_zone_t;
-
-// #define ZONE_NORMAL_ADDR     (0x1000000)   // 16 MB
-// #define ZONE_HIGHMEM_ADDR    (0x38000000)  // 896 MB
 
 // 物理页结构
 typedef struct page_t {
