53501eeec559aec6851b2165418e48059b119f15
zmx976508106 <976508106@qq.com>
2018-12-15 20:00:36 +0800

    虚拟内存管理完成

--- src/include/mm/vmm.h
+++ src/include/mm/vmm.h
@@ -12,20 +12,39 @@
 #ifndef INCLUDE_MM_VMM_H_
 #define INCLUDE_MM_VMM_H_
 
-// 映射 256K 内存所需要的页表条目数
+// 映射剩余内存所需要的页表条目数
 #define PTE_COUNT PAGE_MAX_COUNT
 
-// static pte_t pte_kern[PTE_COUNT];
+pte_t* page_table_p = (pte_t*)PTE_ADDR;
 
 #define le_to_vma(le) list_entry(le, struct vma_struct, list)
 
 // 虚拟内存管理初始化
 void vmm_init(void);
 
-// for (i = 1; i < PTE_COUNT * PTE_SIZE; i++) {
-// 		pte[i] = (i << 12) | PAGE_PRESENT | PAGE_WRITE;
-// 	}
+// 页表条目为4B，即32位
+// PPN 为6位 26-31，VPN 为20位 6-25，status 为2位 4-5，pid 为4位 0-3
 
-// pte_kern[i] = (i << 12) | PAGE_PRESENT | PAGE_WRITE;
+// status
+// 0 - 未分配
+// 1 - 已分配
+// 2 - 保留
+// 3 - 无效
+
+
+uint32_t get_ppn_from_page(uint32_t page_addr);
+uint32_t get_ppn_from_pte(pte_t pte);
+
+// 以下均为物理页号，参数为页框地址
+void set_page_unallocated(uint32_t ppn);
+void set_page_allocated(uint32_t ppn);
+void set_page_reserved(uint32_t ppn);
+void set_page_status(uint32_t ppn, int status);
+int get_page_status(uint32_t ppn);
+void set_page_pid(uint32_t ppn, pid_t pid);
+pid_t get_page_pid(uint32_t ppn);
+
+void vmm_alloc_page(uint32_t page_addr, pid_t pid);
+void vmm_free_page(uint32_t page_addr);
 
 #endif // INCLUDE_MM_VMM_H_
--- src/mm/vmm.c
+++ src/mm/vmm.c
@@ -1,20 +1,129 @@
 #include <include/mm/vmm.h>
 
 static pte_t *pte_addr = (pte_t *)PTE_ADDR;
+
+
 // TODO: 硬件上设置页面大小
-void vmm_init(void){    
-    // 页表数组指针
-    pte_t (*pte_kern)[PTE_COUNT] = (pte_t (*)[PTE_COUNT])pte_addr;
-
-    // 构造页目录(MMU需要的是物理地址，此处需要减去偏移)
-    uint32_t pgd_idx = PGD_INDEX(PAGE_OFFSET);
-    for (uint32_t i = pgd_idx; i < pgd_idx + PTE_COUNT; ++i) {
-            pgd_kern[i] = ((uint32_t)ka_to_pa(pte_kern[i])) | PAGE_PRESENT | PAGE_WRITE;
+void vmm_init(void){
+    /* 设置页表中的有效页为未分配，无效页为保留 */
+    pte_t* pte_p;
+    int cnt = 0;
+    int mask = 0xffffffcf;
+    for (pte_p = page_table_p; pte_p < page_table_p + 0x1000; pte_p++, cnt++)
+    {
+        if(cnt <= PTE_COUNT)
+            *pte_p &= mask;
+        else
+            *pte_p = (*(pte_p) & mask) | 0x02;
     }
+}
 
-    // 构造页表映射，内核 0xC0000000～0xF8000000 映射到 物理 0x00000000～0x38000000 (物理内存前896MB)
-    uint32_t *pte_start = (uint32_t *)(pte_addr + PTE_SIZE * pgd_idx);
-    for (uint32_t i = 0; i < PTE_SIZE * PTE_COUNT; ++i) {
-            pte_start[i] = (i << 12) | PAGE_PRESENT | PAGE_WRITE;
+// 以下均为物理页号
+// status
+// 0 - 未分配
+// 1 - 已分配
+// 2 - 保留
+// 3 - 无效
+uint32_t get_ppn_from_page(uint32_t page_addr){
+    return (page_addr & PAGE_MASK) >> 12;
+}
+
+uint32_t get_ppn_from_pte(pte_t pte){
+    return (pte & 0xfc000000) >> 26;
+}
+
+void set_page_unallocated(uint32_t ppn){
+    set_page_status(ppn, 0);
+}
+void set_page_allocated(uint32_t ppn){
+    set_page_status(ppn, 1);
+}
+void set_page_reserved(uint32_t ppn){
+    set_page_status(ppn, 2);
+}
+
+void set_page_status(uint32_t ppn, int status){
+    if(status < 0 || status > 2){
+        return;
     }
+    pte_t* pte_p;
+    int cnt = 0;
+    for (pte_p = page_table_p; pte_p < page_table_p + 0x1000 && cnt != PTE_COUNT; pte_p++, cnt++)
+    {
+        if(get_ppn_from_pte(*pte_p) == ppn){
+            int mask = 0xffffffcf;
+            status = status << 4;
+            *pte_p = (*(pte_p) & mask) | status;
+            break;
+        }
+    }
+}
+
+int get_page_status(uint32_t ppn){
+    pte_t *pte_p;
+    int cnt = 0;
+    for (pte_p = page_table_p; pte_p < page_table_p + 0x1000 && cnt != PTE_COUNT; pte_p++, cnt++)
+    {
+        if(get_ppn_from_pte(*pte_p) == ppn){
+            int mask = 0xffffffcf;
+            int status = ((*pte_p) & (~mask)) >> 4;
+            return status;
+        }
+    }
+    return -1;
+}
+
+void set_page_pid(uint32_t ppn, pid_t pid){
+    pte_t *pte_p;
+    int cnt = 0;
+    for (pte_p = page_table_p; pte_p < page_table_p + 0x1000 && cnt != PTE_COUNT; pte_p++, cnt++)
+    {
+        if(get_ppn_from_pte(*pte_p) == ppn){
+            int mask = 0xfffffff0;
+            *pte_p = (*(pte_p) & mask) | pid;
+            break;
+        }
+    }
+    assert(false);
+}
+
+pid_t get_page_pid(uint32_t ppn){
+    pte_t *pte_p;
+    int cnt = 0;
+    for (pte_p = page_table_p; pte_p < page_table_p + 0x1000 && cnt != PTE_COUNT; pte_p++, cnt++)
+    {
+        if(get_ppn_from_pte(*pte_p) == ppn){
+            int mask = 0xfffffff0;
+            int pid = *(pte_p) & (~mask);
+            return pid;
+        }
+    }
+    assert(false);
+    return -1;
+}
+
+
+void vmm_alloc_page(uint32_t page_addr, pid_t pid)
+{
+    uint32_t ppn = get_ppn_from_page(page_addr);
+    uint32_t vpn = (ppn + (uint32_t)0x80000000) >> 12;
+    pte_t *pte_p;
+    int cnt = 0;
+    for (pte_p = page_table_p; pte_p < page_table_p + 0x1000 && cnt != PTE_COUNT; pte_p++, cnt++)
+    {
+        if(get_ppn_from_pte(*pte_p) == ppn){
+            *pte_p = 0;
+            *pte_p |= (ppn << 26);
+            *pte_p |= (vpn << 6);
+            *pte_p |= (0x01 << 4);
+            *pte_p |= (pid & 0x0f);
+            return;
+        }
+    }
+}
+
+void vmm_free_page(uint32_t page_addr)
+{
+    uint32_t ppn = get_ppn_from_page(page_addr);
+    set_page_unallocated(ppn);
 }
