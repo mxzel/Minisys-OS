ef6b3126e6da60c36cb8777851be02f3f0012677
zmx976508106 <976508106@qq.com>
2018-12-25 16:26:16 +0800

    sync

--- src/include/mm/mm.h
+++ src/include/mm/mm.h
@@ -7,10 +7,13 @@
 // 内存管理子系统初始化
 void mm_init(void);
 
-// 为指定进程分配页框
-uint32_t alloc_page(pid_t pid, int count);
+// 为指定进程分配内存
+uint32_t alloc_page(pid_t pid, size_t size);
+
+// 将 src 开始的连续的几个页拷贝到 dst 中
+void copy_page(uint32_t src, uint32_t dst, int count);
 
 // 释放指定页框
-void free_page(uint32_t p);
+void free_page(uint32_t p, int count);
 
 #endif // INCLUDE_MM_MM_H_
--- src/mm/mm.c
+++ src/mm/mm.c
@@ -7,10 +7,23 @@ void mm_init(void)
     vmm_init();
 }
 
-uint32_t alloc_page(pid_t pid, int count)
+uint32_t alloc_page(pid_t pid, size_t size)
 {
-    if(count > free_pages_count()){
-        // 物理内存不够
+    /**
+     * 为进程分配内存
+     * pid      : 进程的 id 号
+     * size     : 分配空间的大小
+     */
+    int count = 0;
+    int n_bytes = size;
+    while (n_bytes > 0)
+    {
+        count += 1;
+        n_bytes -= 4096;
+    }
+
+    if(count > free_pages_count() || count == 0){
+        // 物理内存不够用 或者是参数无效
         assert(false);
     }
 
@@ -29,6 +42,17 @@ uint32_t alloc_page(pid_t pid, int count)
     
     // 返回分配的内存空间的首地址（虚拟地址）
     return ret_addr;
+}
+
+
+void copy_page(uint32_t src, uint32_t dst, int count){
+    // 将 src 开始的连续的几个页拷贝到 dst 中（虚拟地址）
+    uint32_t *p_src = src;
+    uint32_t *p_dst = dst;
+    int index;
+    for (index = 0; index < count; ++index){
+        *(p_dst++) = *(p_src++);
+    }
 }
 
 void free_page(uint32_t p, int count)
