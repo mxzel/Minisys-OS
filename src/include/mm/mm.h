78fd83a6ce6ee18167499521ba774b35496a4fef
Stormuzi <3263707107@qq.com>
2018-12-27 20:51:24 +0800

    解决冲突

--- src/include/mm/mm.h
+++ src/include/mm/mm.h
@@ -1,19 +1,18 @@
 #ifndef INCLUDE_MM_MM_H_
 #define INCLUDE_MM_MM_H_
 
-#include <include/mm/pmm.h>
-#include <include/mm/vmm.h>
+#include <mm/pmm.h>
+#include <mm/vmm.h>
 
-uint32_t page_alloc_addrs[64];
-uint8_t page_alloc_counts[64];
-int idx;
+
+
 #define PAGE_SIZE 4096
-=======
+
 extern uint32_t page_alloc_addrs[64];
 extern uint8_t page_alloc_counts[64];
 extern int idx;
 
->>>>>>> origin/minxue
+
 // 地址转换为指针
 void *addr_to_ptr(uint32_t addr);
 
